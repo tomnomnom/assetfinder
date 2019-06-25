d53f404638f90f56b6adec5b6e5b58c66da53f44
hankolsen <henrik@hankolsen.com>
2019-06-25 13:59:24 +0200

    Use both the subdomains and subdomains-aggregate endpoints
    The findsubdomains.com API has two endpoints related subdomains, that each give a subset of the total data.
    
    Call both of them to get as much data as possible.
    
    The subdomains endpoint requires credit to query more than the first page, so it is only guaranteed to give you 30 domains, so as a fallback we can query the subdomains-aggregate endpoint too.

--- findsubdomains.go
+++ findsubdomains.go
@@ -5,17 +5,61 @@ import (
 	"os"
 )
 
-func fetchFindSubDomains(domain string) ([]string, error) {
+var apiToken = os.Getenv("SPYSE_API_TOKEN")
 
-	apiToken := os.Getenv("SPYSE_API_TOKEN")
-	if apiToken == "" {
-		// Must have an API token
-		return []string{}, nil
+func callSubdomainsAggregateEndpoint(domain string) []string {
+	out := make([]string, 0)
+
+	fetchURL := fmt.Sprintf(
+		"https://api.spyse.com/v1/subdomains-aggregate?api_token=%s&domain=%s",
+		apiToken, domain,
+	)
+
+	type Cidr struct {
+		Results []struct {
+			Data struct {
+				Domains []string `json:"domains"`
+			} `json:"data"`
+		} `json:"results"`
 	}
+
+	type Cidrs struct {
+		Cidr16, Cidr24 Cidr
+	}
+
+	wrapper := struct {
+		Cidrs Cidrs `json:"cidr"`
+	}{}
+
+	err := fetchJSON(fetchURL, &wrapper)
+
+	if err != nil {
+		// Fail silently
+		return []string{}
+	}
+
+	for _, result := range wrapper.Cidrs.Cidr16.Results {
+		for _, domain := range result.Data.Domains {
+			out = append(out, domain)
+		}
+	}
+	for _, result := range wrapper.Cidrs.Cidr24.Results {
+		for _, domain := range result.Data.Domains {
+			out = append(out, domain)
+		}
+	}
+
+	return out
+}
+
+/**
+
+ */
+func callSubdomainsEndpoint(domain string) []string {
+	out := make([]string, 0)
 
 	// Start querying the Spyse API from page 1
 	page := 1
-	out := make([]string, 0)
 
 	for {
 		wrapper := struct {
@@ -23,15 +67,16 @@ func fetchFindSubDomains(domain string) ([]string, error) {
 				Domain string `json:"domain"`
 			} `json:"records"`
 		}{}
-		
+
 		fetchURL := fmt.Sprintf(
 			"https://api.spyse.com/v1/subdomains?api_token=%s&domain=%s&page=%d",
 			apiToken, domain, page,
 		)
-	
-		err := fetchJSON(fetchURL, &wrapper)	
+
+		err := fetchJSON(fetchURL, &wrapper)
 		if err != nil {
-			return out, err
+			// Fail silently, by returning what we got so far
+			return out
 		}
 
 		// The API does not respond with any paging, nor does it give us any idea of
@@ -48,6 +93,25 @@ func fetchFindSubDomains(domain string) ([]string, error) {
 
 		page++
 	}
-	
+
+	return out
+}
+
+func fetchFindSubDomains(domain string) ([]string, error) {
+
+	out := make([]string, 0)
+
+	apiToken := os.Getenv("SPYSE_API_TOKEN")
+	if apiToken == "" {
+		// Must have an API token
+		return []string{}, nil
+	}
+
+	// The Subdomains-Aggregate endpoint returns some, but not all available domains
+	out = append(out, callSubdomainsAggregateEndpoint(domain)...)
+
+	// The Subdomains endpoint only guarantees the first 30 domains, the rest needs credit at Spyze
+	out = append(out, callSubdomainsEndpoint(domain)...)
+
 	return out, nil
 }
